972cc4a32263115035d0c5b6e121e03ebee2dae8
nidayede12 <94663772+nidayede12@users.noreply.github.com>
2021-12-06 09:27:17 +0800

    Add files via upload

---
+++ 第四天.py
@@ -0,0 +1,26 @@
+#任务一
+# GDP=[36710.36,35427.10,29863.23,29667.39,27665.36,27650.45,27620.38,25369.20]
+# x=0
+# for i in range(1,9):
+#     x=x+GDP[0]
+# print('城市GDP和为%.2f'%x)
+# print('平均GDP为：%.2f'%(x/i))
+
+
+#任务二
+# a = [1,5,21,30,15,9,30,24]
+# he=0
+# for x in a:
+#     if (x%5)==0:
+#         he+=x
+# print(he)
+
+#任务三
+# list=[1,2,3,4,5,6,7,8,9]
+# print(list[::-1])
+
+#任务四
+# from collections import Counter
+# List = [1,4,7,5,8,2,1,3,4,5,9,7,6,1,10]
+# a = Counter(List)
+# print(a)
